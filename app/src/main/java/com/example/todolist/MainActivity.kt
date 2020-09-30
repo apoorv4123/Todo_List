b22cb40813b859db0d8fdf8f422911bba63ba1a4
gargvishu2000 <45060213+gargvishu2000@users.noreply.github.com>
2020-09-30 15:51:21 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,91 +1,127 @@
 package com.example.todolist
 
+import android.app.AlarmManager
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.graphics.*
-import android.graphics.drawable.Icon
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.app.NotificationCompat
+import androidx.core.view.get
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.todolist.R.menu.main_menu
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.*
 
-//colon used to extend
-//AppCompatActivity is a Base class for activities that wish to use some newer platform feartures on older devices. 
+
 class MainActivity : AppCompatActivity() {
     val list = arrayListOf<TodoModel>()
-    //TodoAdapter is used for recycler view.
-     var adapter = TodoAdapter(list)
+    var adapter = TodoAdapter(list)
 
+    //it provides access to database .
     val db by lazy {
-        //AppDatabase extends RoomDatabase class which ensures the fluent access of database 
         AppDatabase.getDatabase(this)
     }
 
-    //onCreate function starts the activity
+    //function to start activity.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        //todoRv is the id of Recycler View.
+
+
+
+        //todoRv is id of Recycler View.
         todoRv.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = this@MainActivity.adapter
+
         }
-        //initSwipe function performs swiping action, if we swipe left it performs delete and if swiped right then marked as done. 
         initSwipe()
-        //this first clear the previous one data and then add all the data to the list
         db.todoDao().getTask().observe(this, Observer {
             if (!it.isNullOrEmpty()) {
                 list.clear()
                 list.addAll(it)
-                //it will notify the adapter to update data
                 adapter.notifyDataSetChanged()
+                val a=it.get(0).tittle.toString()
+                Log.d("123",a)
             } else {
                 list.clear()
                 adapter.notifyDataSetChanged()
             }
         })
-
-        //Same as onClickListener when a item is clicked it gets the id and match the id and perform the respective function
-        //Three dots(
-        // .
-        // .
-        // . ) it shows history in this
     }
 
-   // override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-     //   menuInflater.inflate(R.menu.main_menu,menu)
-       // return super.onCreateOptionsMenu(menu)
-    //}
-    //override fun onOptionsItemSelected(item: MenuItem): Boolean {
-      //  when (item.itemId) {
-        //    R.id.history -> {
-          //      startActivity(Intent(this, HistoryActivity::class.java))
-            //}
-        //}
-        //return super.onOptionsItemSelected(item)
-    //}
-    
-    //when + action button is clicked then TaskActivity comes into play.
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        val item = menu?.findItem(R.id.search)
+        val searchView = item?.actionView as SearchView
+        item.setOnActionExpandListener(object :MenuItem.OnActionExpandListener{
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                displayTodo()
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                displayTodo()
+                return true
+            }
+
+        })
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if(!newText.isNullOrEmpty()){
+                    displayTodo(newText)
+                }
+                return true
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun displayTodo(newText : String=" ")
+    {
+        db.todoDao().getTask().observe(this, Observer {
+            if(it.isNotEmpty())
+            {
+                list.clear()
+                list.addAll(
+                    it.filter { todo->
+                        todo.tittle.contains(newText,true)
+                    }
+                )
+                adapter.notifyDataSetChanged()
+            }
+        })
+
+    }
+
     fun openNewTask(view: View) {
-        //Intent is used to invoke a activity.
         startActivity(Intent(this, TaskActivity::class.java))
     }
 
-    //function initSwipe
     private fun initSwipe() {
         val simpleItemTouchCallback = object : ItemTouchHelper.SimpleCallback(
-            //If we don't want to add up and down action action then set it's value to 0.
             0,
             ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
         ) {
@@ -98,21 +134,17 @@ class MainActivity : AppCompatActivity() {
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.adapterPosition
 
-                //GlobalScope is used to perform work in background.
                 if (direction == ItemTouchHelper.LEFT) {
                     GlobalScope.launch(Dispatchers.IO){
-                        //Swiped left perform delete task action.
                         db.todoDao().deleteTask(adapter.getItemId(position))
                     }
                 } else if (direction == ItemTouchHelper.RIGHT) {
                     GlobalScope.launch(Dispatchers.IO){
-                        //Swiped right mark as done
                         db.todoDao().finishTask(adapter.getItemId(position))
                     }
                 }
             }
-\
-            //fun to place icon
+
             override fun onChildDraw(
                 canvas: Canvas,
                 recyclerView: RecyclerView,
@@ -121,72 +153,95 @@ class MainActivity : AppCompatActivity() {
                 dY: Float,
                 actionState: Int,
                 isCurrentlyActive: Boolean
-            ) {
-                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
-                    val itemView = viewHolder.itemView
+            ) = if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                val itemView = viewHolder.itemView
 
-                    val paint = Paint()
-                    val icon: Bitmap
+                val paint = Paint()
+                val icon: Bitmap?
 
-                    if (dX > 0) {
-
-                        //check_black is icon 
-                        icon = BitmapFactory.decodeResource(resources, R.mipmap.ic_check_black)
-                        
-                        //Putting background color as green when swiped right
-                        paint.color = Color.parseColor("#388E3C")
-
-                        //Drwaing Rectangle
-                        canvas.drawRect(
-                            itemView.left.toFloat(), itemView.top.toFloat(),
-                            itemView.left.toFloat() + dX, itemView.bottom.toFloat(), paint
-                        )
-
+                //if it is moved right then marked it done.
+                if (dX > 0) {
+                    icon=getBitmap(this@MainActivity,R.mipmap.ic_check_black)
+                    paint.color = Color.parseColor("#388E3C")
+                    //Drawing rectangle
+                    canvas.drawRect(
+                        itemView.left.toFloat(), itemView.top.toFloat(),
+                        itemView.left.toFloat() + dX, itemView.bottom.toFloat(), paint
+                    )
+                    if (icon != null) {
                         canvas.drawBitmap(
                             icon,
-                            //Position of icon
                             itemView.left.toFloat(),
+                            //position of the check black.
                             itemView.top.toFloat() + (itemView.bottom.toFloat() - itemView.top.toFloat() - icon.height.toFloat()) / 2,
                             paint
                         )
                     }
-                    else if(dX<0)
-                    {
-                        icon=BitmapFactory.decodeResource(resources,R.mipmap.ic_cancel_black)
-                        //Red color
-                        paint.color=Color.parseColor("#D32F2F")
+                }
+                //if it is moved left
+                else if(dX<0)
+                {
+                    icon=getBitmap(this@MainActivity,R.mipmap.ic_cancel_black)
+                    paint.color=Color.parseColor("#D32F2F")
 
-                        //Drawing rectangle
-                        canvas.drawRect(itemView.right.toFloat()+dX,itemView.top.toFloat(),
-                            itemView.right.toFloat(),itemView.bottom.toFloat(),paint)
+                    //drawing rectangle
+                    canvas.drawRect(itemView.right.toFloat()+dX,itemView.top.toFloat(),
+                        itemView.right.toFloat(),itemView.bottom.toFloat(),paint)
 
+                    if (icon != null) {
                         canvas.drawBitmap(
                             icon,
-                            //Delete icon position
+                            //position of delete icon.
                             itemView.right.toFloat()-icon.width,
                             itemView.top.toFloat() + (itemView.bottom.toFloat() - itemView.top.toFloat() - icon.height.toFloat()) / 2,
                             paint
                         )
                     }
-                    //perform translation.
-                   viewHolder.itemView.translationX=dX
-
-                } else {
-                    super.onChildDraw(
-                        canvas,
-                        recyclerView,
-                        viewHolder,
-                        dX,
-                        dY,
-                        actionState,
-                        isCurrentlyActive
-                    )
                 }
+               viewHolder.itemView.translationX=dX
+
+            } else {
+                super.onChildDraw(
+                    canvas,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
             }
         }
         val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
         itemTouchHelper.attachToRecyclerView(todoRv)
+    }
 
+    //converting vector image to bitmap.
+    private fun getBitmap(context: Context, vectorDrawableId: Int): Bitmap? {
+        var bitmap: Bitmap? = null
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            val vectorDrawable: Drawable? = context.getDrawable(vectorDrawableId)
+            if (vectorDrawable != null) {
+                bitmap = Bitmap.createBitmap(
+                    vectorDrawable.intrinsicWidth,
+                    vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888
+                )
+            }
+            val canvas = bitmap?.let { Canvas(it) }
+            if (vectorDrawable != null) {
+                if (canvas != null) {
+                    vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+                }
+            }
+            if (canvas != null) {
+                if (vectorDrawable != null) {
+                    vectorDrawable.draw(canvas)
+                }
+            }
+        } else {
+            bitmap = BitmapFactory.decodeResource(context.getResources(), vectorDrawableId)
+        }
+        return bitmap
     }
 
 }
